9f9489792a46b77010a059a55bc14fdd8bbf36be
Kzdika <115381989+Kzdika@users.noreply.github.com>
2022-10-29 03:24:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,39 +3,39 @@
 // JANGAN DIHAPUS KALAU MAU DITAMBAHIN
 
 // NUMBER
-global.owner = ['62895327934887'] 
+global.owner = ['6288293499014'] 
 global.mods = [] 
 global.prems = [] 
-global.kontak = ['62895327934887']
+global.kontak = ['6288293499014']
 
 //  INFO BOT 
-global.naown = 'KRIZYN OFC'
-global.noown = '62895327934887'
-global.namebot = 'FORYN FOUR'
-global.nobot = '6289502319715'
+global.naown = 'Kz'
+global.noown = '6288293499014'
+global.namebot = 'kzbotz'
+global.nobot = '6287776985031'
 
 //  URL SOSMED
-global.instagram = 'https://instagram.com/itskri99'
+global.instagram = 'https://instagram.com/rhandrqz'
 global.github = 'https://github.com/krizynofc'
 global.gc = 'https://chat.whatsapp.com/HVyRaNWp18NKMAIHv8NjLP'
 global.web = 'https://lynk.id/kri.com' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
 global.saweria = 'https://saweria.co/Kricom'
 
 // PAYMANT 
-global.dana = '081360482998'
-global.pulsa = '081360482998'
-global.gopay = '081360482998'
-global.shopay = '081360482998'
-global.ovo = '081360482998'
+global.dana = '088293499014'
+global.pulsa = '087776985031'
+global.gopay = '088293499014'
+global.shopay = '-_-'
+global.ovo = '-_-'
 
 // PROFILE ALL 
 global.ppreg = 'https://telegra.ph/file/445721f7cdab701543840.jpg'
-global.fotonya1 = 'https://telegra.ph/file/c58f43806ef4b7038a7f4.jpg' //ganti jadi foto bot mu
-global.fotonya2 = 'https://telegra.ph/file/c58f43806ef4b7038a7f4.jpg' //ini juga ganti 
-global.ppacc = 'https://telegra.ph/file/a077271ff372545599dbc.jpg'
-global.imgloc = 'https://telegra.ph/file/e6355ade20840c96814ed.jpg'
+global.fotonya1 = 'https://telegra.ph/file/2d461fcccf26557c2df2b.jpg' //ganti jadi foto bot mu
+global.fotonya2 = 'https://telegra.ph/file/5b4498265f3e8c9be8c75.jpg' //ini juga ganti 
+global.ppacc = 'https://telegra.ph/file/074ac061bc8409689d872.jpg'
+global.imgloc = 'https://telegra.ph/file/ea8fefb5debafc0031463.jpg'
 global.media = 'https://telegra.ph/file/f209153a54698c43032f7.jpg'
-global.ppm = 'https://telegra.ph/file/85f5dd4cfb4fdd9d65cbf.jpg'  // bagian atas all command
+global.ppm = 'https://telegra.ph/file/2d461fcccf26557c2df2b.jpg' // bagian atas all command
 
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 
@@ -80,6 +80,7 @@ global.admen = `┏═┅〔 *SUPPORT* 〕┅═✦
 ┃⫹⫺ Nurutomo
 ┃⫹⫺ Ilmanhdyt
 ┃⫹⫺ Elyas
+┃⫹⫺ Kz
 ┃⫹⫺ Hyzer
 ┃⫹⫺ KrizynOfc
 ┃⫹⫺ Zivsan
@@ -105,7 +106,7 @@ global.salah = 'Salah ❌\n'
 // WATERMARK STICKER 
 global.stiker_wait = '⏳ Stiker sedang Proses'
 global.packname = 'FORYN FOUR'
-global.author = 'By krizynOfc'
+global.author = 'By kz'
 
 //  APIKEY ANDA 
 global.lolkey = 'Papah-Chan' //biar mudah ngegantinya semisal apikeynya expired:v
